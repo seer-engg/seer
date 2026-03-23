404762a24017d71042e4cc0c03ecc0ee65d4072c
lokesh-danu <lokeshdanu9@gmail.com>
2026-03-23 12:55:21 +0000

    feat: rich text editor

---
+++ src/seer/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Utility modules for Seer."""
+
+from seer.utils.rich_text import Platform, RichTextConverter, convert_rich_text
+
+__all__ = ["Platform", "RichTextConverter", "convert_rich_text"]
